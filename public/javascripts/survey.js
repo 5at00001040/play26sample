7ca4ebd16b1b640111d51dae82763795afb1e31b
Sato <5at0000.10.40@gmail.com>
2017-10-29 09:34:57 +0900

    change view layout

--- public/javascripts/survey.js
+++ public/javascripts/survey.js
@@ -1,52 +1,46 @@
 var saList = new Vue({
   el: '#sa-app',
   data: {
-    question: "質問",
-    choice1: "選択肢1",
-    choice2: "選択肢2",
-    choice3: "選択肢3",
-    choice4: "選択肢4",
-    choice5: "選択肢5",
+    question: "質問を入力してください",
+    choice1: "選択肢を入力してください",
+    choice2: "選択肢を入力してください",
+    choice3: "選択肢を入力してください",
+    choice4: "選択肢を入力してください",
+    choice5: "選択肢を入力してください",
     questionIdList: [],
     selectQuestionId: 0,
     questionData: {},
-    answerData: {},
-    picked: "",
-    res: [],
-    interval: null,
+    answerData: {}
   },
   mounted: function () {
-    this.loadQuestionIdList()
-    this.loadData()
+    this.pullQuestionIdList()
+//    this.selectQuestionId = this.questionIdList[0].id
+//    this.pullQuestion()
+//    this.pullAnswer()
   },
   methods: {
-    loadQuestionIdList: function () {
+    pullQuestionIdList: function () {
       axios.get("http://localhost:9000/sa")
       .then(response => {this.questionIdList = response.data.res})
     },
-    loadQuestion: function () {
+    pullQuestion: function () {
       axios.get("http://localhost:9000/sa/" + this.selectQuestionId)
       .then(response => {this.questionData = response.data.res[0]})
     },
-    loadAnswer: function () {
+    pullAnswer: function () {
       axios.get("http://localhost:9000/sa/answer/" + this.selectQuestionId)
       .then(response => {this.answerData = response.data})
     },
     changeQuestion: function () {
-        this.loadQuestion()
-        this.loadAnswer()
+        this.refresh()
     },
-    loadData: function () {
-        axios.get("http://localhost:9000/sa")
-        .then(response => {this.res = response.data.res})
-    },
-    addData: function () {
+    pushQuestion: function () {
         var param = {req: {question: this.question, choice1: this.choice1, choice2: this.choice2, choice3: this.choice3, choice4: this.choice4, choice5: this.choice5}}
         axios.defaults.headers['X-Requested-With'] = 'XMLHttpRequest'
         axios.post("http://localhost:9000/sa", param)
-        this.loadData()
+        this.refresh()
     },
-    answer: function (qid, radioName) {
+    pushAnswer: function (qid, radioName) {
         var elements = document.getElementsByName(radioName)
         for (var checked = "", i=elements.length; i--;) {
             if ( elements[i].checked ) {
@@ -57,23 +51,17 @@ var saList = new Vue({
         var param = {req: {questionId: qid, choice: Number(checked)}}
         axios.defaults.headers['X-Requested-With'] = 'XMLHttpRequest'
         axios.post("http://localhost:9000/sa/answer", param)
-        this.loadData()
-        this.loadQuestion()
-        this.loadAnswer()
+        this.refresh()
+    },
+    refresh: function () {
+        this.pullQuestionIdList()
+        this.pullQuestion()
+        this.pullAnswer()
     },
     resetData: function () {
         axios.defaults.headers['X-Requested-With'] = 'XMLHttpRequest'
         axios.post("http://localhost:9000/reset")
-        this.loadData()
+        this.refresh()
     }
-  },
-  created: function () {
-    this.loadData()
-    this.interval = setInterval(function () {
-        this.loadData()
-    }.bind(this), 10000)
-  },
-  beforeDestroy: function(){
-    clearInterval(this.interval);
-  }.bind(this)
+  }
 });
